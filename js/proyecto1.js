63edaf31f1ecc5456b9e93f054c7eea13330b536
DEREK-SNEIDER <esnei1595@gmail.com>
2022-07-26 00:12:16 -0500

    se agrega el proyecto

---
+++ js/proyecto1.js
@@ -0,0 +1,18 @@
+
+
+
+
+let nombre = "Arturo",
+edad = 18;
+
+
+if (edad == 18){
+    alert("Bienvenido, tienes 18");    // aviso por alerta
+console.log("Bienvenido, Arturo");   //aviso por consola
+}else if (nombre == "Arturo") {
+alert("Bienvenido" + nombre) ;
+}else {
+    alert( "No tienes 18 años");
+console( "No tienes 18 años");
+
+}
