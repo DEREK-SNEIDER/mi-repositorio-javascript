40361d1db65abdceab92236e43118b1b8102d982
DEREK-SNEIDER <esnei1595@gmail.com>
2022-07-30 21:07:11 -0500

    incluy un ciclo

--- js/proyecto1.js
+++ js/proyecto1.js
@@ -1,18 +1,21 @@
-
-
-
-
 let nombre = "Arturo",
-edad = 18;
+    edad = 18;
 
 
-if (edad == 18){
-    alert("Bienvenido, tienes 18");    // aviso por alerta
-console.log("Bienvenido, Arturo");   //aviso por consola
-}else if (nombre == "Arturo") {
-alert("Bienvenido" + nombre) ;
-}else {
-    alert( "No tienes 18 años");
-console( "No tienes 18 años");
-
+if (edad == 18) {
+    alert("Bienvenido, tienes 18"); // aviso por alerta
+    console.log("Bienvenido, Arturo"); //aviso por consola
+} else if (nombre == "Arturo") {
+    alert("Bienvenido" + nombre);
+} else {
+    alert("No tienes 18 años");
+    console("No tienes 18 años");
 }
+
+
+    for (let i = 1; i <= 5; i++) {
+
+        let ingresarNombre = prompt("Ingresar nombre");
+
+        alert(" Turno  N° " + i + " Nombre: " + ingresarNombre);
+    } 
