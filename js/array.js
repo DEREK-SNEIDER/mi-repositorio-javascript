a31cb707481494cbb363541461929727c446f55f
DEREK-SNEIDER <esnei1595@gmail.com>
2022-08-08 23:54:35 -0500

    se agrega ejercicios con array y html

--- js/array.js
+++ js/array.js
@@ -1,5 +1,5 @@
-
-
+/*
+// EJERCIO #1 //
 window.onload = function () {
 
     //console.log('Hola');
@@ -42,4 +42,68 @@ window.onload = function () {
 
     console.log(`El numero menor es ${menor}`);
 
+}*/
+
+
+/*
+//     EJERCICIO #2    //
+const ciudades = ["Colombia", "Cesar", "Barcelona", "La jagua"];
+
+if(ciudades.includes("Barcelona")) {
+   console.log("Barcelona esta en la lista");
+} else {
+    console.log("barcelona no esta en la lista");
 }
+
+var encontrada = ciudades.find(function(ciudad) {  
+    return ciudad[0] == "B";
+});
+ 
+if(encontrada) {
+    console.log("hay una ciudads que empieza por B"); 
+} else {
+    console.log("no hay ciudad que empieza por B")
+}
+ 
+console.log(encontrada); 
+
+*/
+
+// EJERCICIO #3
+const ciudades = [
+    {
+        name: "Madrid",
+        population: "3M",
+        country: "Spain",
+    },
+    {
+        name: "Paris",
+        population: "10M",
+        country: "France",
+    },
+    {
+        name: "Florianopolis",
+        population: "600K",
+        country: "Brasil",
+    }
+];
+
+
+var ciudad = ciudades.find(function(current) {
+    return current.name === "paris";
+});
+
+if(ciudad){
+    console.log(ciudad.population);
+} else {
+    console.log("no se encontro la ciudad")
+}
+
+
+
+
+
+
+
+
+
